ebdf642d7fb24285800186139b74c4b4c82e065a
Bob Farrell <git@bob.frl>
2022-06-13 20:52:43 +0100

    Improve email report styling

--- spec/support/example_report_data.rb
+++ spec/support/example_report_data.rb
@@ -2,5 +2,6 @@
 
 # Example report data record used for testing query output.
 class ExampleReportData < ReportRecord
+  establish_connection(Rails.application.config_for('database.reports'))
   self.table_name = 'example_table'
 end
